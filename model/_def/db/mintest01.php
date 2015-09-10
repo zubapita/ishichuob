e21cb530351dc0c5d5d3a1b2b0563ff2218f2e4c
zubapita <tomoyun@zubapita.jp>
2015-09-11 07:16:23 +0900

    Add new app.

---
+++ model/_def/db/mintest01.php
@@ -0,0 +1,7 @@
+<?php
+class mintest01 extends DBSpec {
+	public static $SYSTEM = 'mysql';
+	public static $DSN = 'mysql:dbname=mintest01;host=127.0.0.1';
+	public static $USER = 'mintest01';
+	public static $PASSWORD = 'mintest01..';
+}
